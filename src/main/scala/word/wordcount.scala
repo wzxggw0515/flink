c45fc00446f4e9f7b478821e23a63b4272700dff
e <wzxggw@sina.com>
2020-02-22 15:23:26 +0800

    init

--- src/main/scala/word/wordcount.scala
+++ src/main/scala/word/wordcount.scala
@@ -1,11 +1,16 @@
 package word
 
+import org.apache.flink.api.scala.ExecutionEnvironment
+
 object wordcount {
 
   def main(args: Array[String]): Unit = {
-
-
-
+   val env:ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+   val  input  = "D:\\tmp\\a.txt"
+   import  org.apache.flink.api.scala._
+   val dataset:DataSet[String] = env.readTextFile(input);
+   val aggr = dataset.flatMap(_.split("")).map((_,1)).groupBy(0).sum(1)
+  aggr.print();
 }
 
 }
